0480951a9ee7f8c52715237184b8de0ee841de2e
teneciajennings <jenningst82@gmail.com>
2018-06-13 11:09:34 -0500

    creatMyObject complete

---
+++ drills1.js
@@ -0,0 +1,13 @@
+'use strict';
+function createMyObject() {
+  let newObject = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello : function () {
+      return 'hello';
+    }
+  };
+
+  return newObject;
+}
